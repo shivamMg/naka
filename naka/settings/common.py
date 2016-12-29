79a45d2f03f664679c330d054f69d42f0f77fc9b
Shivam Mamgain <shivammamgain47@gmail.com>
2016-12-29 16:53:44 +0530

    Separate settings for Dev and Prod

--- naka/settings/common.py
+++ naka/settings/common.py
@@ -2,7 +2,7 @@ import os
 import json
 import datetime
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 
 try:
